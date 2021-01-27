3bf5220de326b504d040468f601773699c80a569
trnguyen1510 <trnguyen1510@gmail.com>
2021-01-26 20:06:10 -0800

    adding app

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,69 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
+import Search from "./Search";
 
-export default function Weather (props) {
-
-    function handleResponse(response) {
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`)
-    }
-    let apiKey = "d80253f7bab7ebfb0d574948c8b564be";
-    let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiURL).then(handleResponse);
+export default function Weather() {
+    let weatherData = {
+        city: "San Diego",
+        temp: "19",
+        humidity: "10",
+        precipitation: "1",
+        wind: "5",
+        weatherIcon:
+            "https://cdn2.iconfinder.com/data/icons/weather-icons-8/512/sunny-512.png"
+    };
     return (
-        <h2>Hello from Weather</h2>
-    )
+        <div className="weather">
+            <Search />
+
+            <div className="row">
+                <div className="col-6">
+                    <div className="clearfix weather-temp">
+                        <img
+                            src={weatherData.weatherIcon}
+                            alt=""
+                            className="icon-weather float-left"
+                        />
+                        <div className="float-left">
+                            <span className="temperature">{weatherData.temp}</span>
+                            <span className="change-temp">
+                                <a href="/">°C</a>|
+                <a href="/" className="active">
+                                    °F
+                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation:{weatherData.precipitation}%</li>
+                        <li>Humidity: {weatherData.humidity}%</li>
+                        <li>Wind: {weatherData.wind} km/h</li>
+                    </ul>
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                        <button
+                            type="button"
+                            className="form-control btn btn-currentlocation"
+                        >
+                            Temperature
+            </button>
+                        <button
+                            type="button"
+                            className="form-control btn btn-currentlocation"
+                        >
+                            Precipitation
+            </button>
+                        <button
+                            type="button"
+                            className="form-control btn btn-currentlocation"
+                        >
+                            Wind
+            </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
