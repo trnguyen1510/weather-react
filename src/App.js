959cbf6a2d65ec15456ecc8e39f19b9564996565
trnguyen1510 <trnguyen1510@gmail.com>
2021-01-27 20:43:42 -0800

    adding boostrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 
+
 export default function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Search from "./Search";
-import "./App.css";
+import "./index.css";
 import "./Weather.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <div className="weather">
         <Search />
+      </div>
+      <div className="footnote">
+        <small>
+          This project is coded by <a href="https://www.linkedin.com/in/tramnguyen1510/">Tram Tracy Nguyen
+
+        </a>
+        and is
+        <a href="https://github.com/trnguyen1510/weather-react">open-sourced on GitHub
+        </a>
+        and
+        <a href="https://tracy-weather-reactapp.netlify.app">hosted on Netlify
+        </a>
+        </small>
       </div>
     </div>
   );
